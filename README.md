2167f5597aba00c0ee178daecebc4650571c5b8d
bhavyesharma <bhavye.sharma1996@gmail.com>
2018-04-27 01:07:18 +0530

    first Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### First REACT project
+
+#Youtube API search
+#as seen in React with Redux by stephen grinder-udemy
+```
+> npm install
+> npm start
+```
+
---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react';
+ 
+
+class SearchBar extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={term:''};
+    }
+
+    render(){
+    return (
+        <div className="search-bar">
+            <input 
+            value={this.state.term}
+            onChange={e => this.onInputChange(e.target.value)}/>
+           
+        </div>
+        );
+    }
+    
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+
+    }
+// onInputChange(e){
+//     console.log(e.target.value);
+// }
+
+}
+
+export default SearchBar;
